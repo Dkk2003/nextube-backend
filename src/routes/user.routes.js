e67027ed49b101db7a328920bce4d0d944df864c
Dkk2003 <darshan1010prajapati@gmail.com>
2024-06-29 20:26:02 +0530

    create api for refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -22,5 +23,6 @@ router.route("/login").post(loginUser);
 //secured routes
 
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
