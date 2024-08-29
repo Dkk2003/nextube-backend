bf2601bb39893ece916f80bdb726579e564285f3
Dkk2003 <darshan1010prajapati@gmail.com>
2024-08-30 00:05:30 +0530

    comments all api are done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -25,4 +25,95 @@ const getVideoComments = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, comments, "Fetch All Comments Successfully"));
 });
 
-export { getVideoComments };
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId, content } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  if (!content) {
+    throw new ApiError(400, "Comment content are empty");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(400, "There was a problem while add comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment added to video"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { videoId, commentId, newContent } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+
+  if (!newContent) {
+    throw new ApiError(400, "Comment content are empty");
+  }
+
+  const comment = await Comment.findOneAndUpdate(
+    {
+      _id: commentId,
+      video: videoId,
+      owner: req.user?._id,
+    },
+    {
+      $set: {
+        content: newContent,
+      },
+    },
+    { new: true }
+  );
+
+  if (!comment) {
+    throw new ApiError(400, "There was a problem while update comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment updated successfully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { videoId, commentId } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+
+  const comment = await Comment.findOneAndDelete(
+    {
+      _id: commentId,
+      video: videoId,
+      owner: req.user?._id,
+    },
+    { new: true }
+  );
+
+  if (!comment) {
+    throw new ApiError(400, "There was a problem while update comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { getVideoComments } from "../controllers/comment.controller.js";
+import {
+  addComment,
+  deleteComment,
+  getVideoComments,
+  updateComment,
+} from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.use(verifyJwt);
 
 router.route("/getVideoComments/:videoId").get(getVideoComments);
+router.route("/addComment").post(addComment);
+router.route("/updateComment").patch(updateComment);
+router.route("/deleteComment").delete(deleteComment);
 
 export default router;
